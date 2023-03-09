858cf1b7b5851d9b476fe12acf09205728ec767d
suryamichael <112886206+suryamichael@users.noreply.github.com>
2023-03-09 21:16:44 +0700

    Delete List2Array.cs

--- List2Array.cs
+++
@@ -1,9 +0,0 @@
- int[] NumberArray = {15, 10, 17, 2, 4};
-
-List<int> BasicNumber = new List<int>();
-
-BasicNumber.AddRange(NumberArray); 
-
-int[] Number = BasicNumber.ToArray();
-foreach (int x in Number) 
-Console.WriteLine(x);
